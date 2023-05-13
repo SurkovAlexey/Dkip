4713a5ce37f13256c74e19edccd356025adfd3fa
Alexey Surkov <133164655+SurkovAlexey@users.noreply.github.com>
2023-05-14 02:07:48 +0300

    Пятница 13

---
+++ datetime.pas
@@ -0,0 +1,13 @@
+﻿##
+var year := ReadInteger;
+var dt := new DateTime(year,1,1);
+var nd := Ord(dt.DayOfWeek); 
+dt := dt.AddDays((12 - nd) mod 7);
+while dt.Year < year+1 do
+begin
+if dt.Day=13 then 
+  begin
+ $'13.{dt.month:d2}'.Print
+    end;
+dt := dt.AddDays(7);
+end;
