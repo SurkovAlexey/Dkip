eb58334a2d10fe31c8807c91dd134fea83108ee9
Alexey Surkov <133164655+SurkovAlexey@users.noreply.github.com>
2023-05-18 05:12:38 +0300

    Add files via upload

---
+++ перев.pas
@@ -0,0 +1,11 @@
+﻿##
+var s := ReadString; 
+s:= s.ToLower;
+for  var i:= length(s) downto 1 do
+if ((not(s[i] in ['a'..'z'])) and (not(s[i]in ['а'..'я'] ))) or ((s[i]='ё'))  then delete(s,i,1); 
+var s1:=s.Inverse;
+var k:=0;
+for var i:= length(s) downto 1 do
+  if s[i]=s1[i] then k:=k+0
+else k:=k+1;
+println(k=0);
